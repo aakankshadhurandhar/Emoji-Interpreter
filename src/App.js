3d4fec395c3f34c802e9a500577c01c8e45951ad
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-03-12 17:42:58 +0530

    Done with app
    Added dictionary .understood the concept of dictionary in react.Learned how to input,process and display data in react.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+const emojiDictionary={
+ "🤣":"Laughing",
+  "😎":"Smiling with sunglasses",
+  "😠":"Angry face",
+  "👍":"thumbs Up",
+  " ":"enter something"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+};
+//const emojis=Object.keys(emojiDictionary);
+
+
+export default function App(){
+  var[meaning,setmeaning]=useState("");
+  function emojihandler(event) {
+    var meaning=emojiDictionary[event.target.value];
+    if(meaning===undefined)
+    {meaning="emoji not in database";}
+    setmeaning(meaning);
+    
+  }
+  
+
+
+
+
+  return ( <div>
+    <nav>
+        <div className="navbar">Emoji Interpreter 😊  </div>
+    </nav>
+    
+    
+   <div className="main">
+   <input onChange={emojihandler} ></input>
+    <h3>We know following emojis</h3>
+    <h4>🤣😎😠👍</h4>
+
+    <h2>{meaning}</h2>
+    </div>
+    
     </div>
   );
 }
 
-export default App;
